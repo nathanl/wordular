91526b4bb8d0e4a55c2801e901bef53353223086
Nathan Long <him@nathanmlong.com>
2016-05-14 06:38:55 -0400

    Dictionary loader rejects some garbage words

--- web/models/anagrams.ex
+++ web/models/anagrams.ex
@@ -16,8 +16,15 @@ defmodule Anagrams do
   def load_human_readable_dictionary(filename) do
     File.stream!(filename)
     |> Enum.map(&String.strip/1)
-    # Don't consider "" or "u" or "k" to be valid words
-    |> Enum.reject(&(String.length(&1) < 2 && !&1 in ~w(a i o)))
+    # Throw out a bunch of tiny garbage "words" which vastly increase the
+    # number of anagrams if included
+    |> Enum.filter(fn (word) ->
+      cond do
+        String.length(word) < 2 && !word in ~w(a i o) -> false
+        String.length(word) == 2 && (!word in ~w(ad ah ai am an as at aw ax ay be bi by do eh er ex go ha he hi ho id if in is it la lo ma me my of oh on or ow ox oy pa pH pi qi re so to um up us we ye yo)) -> false
+        true -> true
+      end
+    end)
   end
 
   # Sorted, non-unique list of codepoints
